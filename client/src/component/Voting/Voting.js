179e7e9c661ed84b19103a84ee65fd7cf4b49cca
iamzmeirambek <Iamzmeirambek@gmail.com>
2023-05-12 21:56:55 +0600

    Metamask connect button

--- client/src/component/Voting/Voting.js
+++ client/src/component/Voting/Voting.js
@@ -10,10 +10,6 @@ import NotInit from "../NotInit";
 // Contract
 import getWeb3 from "../../getWeb3";
 import Election from "../../contracts/Election.json";
-import { abi, NFT_CONTRACT_ADDRESS } from "./constant";
-
-
-
 
 // CSS
 import "./Voting.css";
@@ -37,7 +33,6 @@ export default class Voting extends Component {
         hasVoted: false,
         isVerified: false,
         isRegistered: false,
-        isHasNFT: false,
       },
     };
   }
@@ -58,8 +53,8 @@ export default class Voting extends Component {
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = Election.networks[networkId];
       const instance = new web3.eth.Contract(
-        Election.abi,
-        deployedNetwork && deployedNetwork.address
+          Election.abi,
+          deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -72,8 +67,8 @@ export default class Voting extends Component {
 
       // Get total number of candidates
       const candidateCount = await this.state.ElectionInstance.methods
-        .getTotalCandidate()
-        .call();
+          .getTotalCandidate()
+          .call();
       this.setState({ candidateCount: candidateCount });
 
       // Get start and end values
@@ -85,8 +80,8 @@ export default class Voting extends Component {
       // Loading Candidates details
       for (let i = 1; i <= this.state.candidateCount; i++) {
         const candidate = await this.state.ElectionInstance.methods
-          .candidateDetails(i - 1)
-          .call();
+            .candidateDetails(i - 1)
+            .call();
         this.state.candidates.push({
           id: candidate.candidateId,
           header: candidate.header,
@@ -97,208 +92,175 @@ export default class Voting extends Component {
 
       // Loading current voter
       const voter = await this.state.ElectionInstance.methods
-        .voterDetails(this.state.account)
-        .call();
-      
+          .voterDetails(this.state.account)
+          .call();
+      this.setState({
+        currentVoter: {
+          address: voter.voterAddress,
+          name: voter.name,
+          phone: voter.phone,
+          hasVoted: voter.hasVoted,
+          isVerified: voter.isVerified,
+          isRegistered: voter.isRegistered,
+        },
+      });
+
       // Admin account and verification
       const admin = await this.state.ElectionInstance.methods.getAdmin().call();
       if (this.state.account === admin) {
         this.setState({ isAdmin: true });
       }
-      
-      const hasNFT = await this.checkNFT();
-      if (hasNFT === true){       
-        //alert("You have the required NFT to continue."); 
-        this.setState({
-          currentVoter: {
-            address: voter.voterAddress,
-            name: voter.name,
-            phone: voter.phone,
-            hasVoted: voter.hasVoted,
-            isVerified: voter.isVerified,
-            isRegistered: voter.isRegistered,
-            isHasNFT: true,
-          },
-        });
-      }
-      else{
-        this.setState({
-          currentVoter: {
-            address: voter.voterAddress,
-            name: voter.name,
-            phone: voter.phone,
-            hasVoted: voter.hasVoted,
-            isVerified: voter.isVerified,
-            isRegistered: voter.isRegistered,
-            isHasNFT: false,
-          },
-        });
-      }
-      
-
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
+          `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
-  }
-  
-  async checkNFT() {
-    const contractAddress = NFT_CONTRACT_ADDRESS; 
-    const contractABI = abi; 
-    const web3 = this.state.web3;
-    const account = this.state.account;
-  
-    const nftContract = new web3.eth.Contract(contractABI, contractAddress);
-    const balance = await nftContract.methods.balanceOf(account).call();
-  
-    return balance > 0;
-  }  
+  };
 
   renderCandidates = (candidate) => {
     const castVote = async (id) => {
       await this.state.ElectionInstance.methods
-        .vote(id)
-        .send({ from: this.state.account, gas: 1000000 });
+          .vote(id)
+          .send({ from: this.state.account, gas: 1000000 });
       window.location.reload();
     };
     const confirmVote = (id, header) => {
       var r = window.confirm(
-        "Vote for " + header + " with Id " + id + ".\nAre you sure?"
+          "Vote for " + header + " with Id " + id + ".\nAre you sure?"
       );
       if (r === true) {
         castVote(id);
       }
     };
     return (
-      <div className="container-item">
-        <div className="candidate-info">
-          <h2>
-            {candidate.header} <small>#{candidate.id}</small>
-          </h2>
-          <p className="slogan">{candidate.slogan}</p>
+        <div className="container-item">
+          <div className="candidate-info">
+            <h2>
+              {candidate.header} <small>#{candidate.id}</small>
+            </h2>
+            <p className="slogan">{candidate.slogan}</p>
+          </div>
+          <div className="vote-btn-container">
+            <button
+                onClick={() => confirmVote(candidate.id, candidate.header)}
+                className="vote-bth"
+                disabled={
+                    !this.state.currentVoter.isRegistered ||
+                    !this.state.currentVoter.isVerified ||
+                    this.state.currentVoter.hasVoted
+                }
+            >
+              Vote
+            </button>
+          </div>
         </div>
-        <div className="vote-btn-container">
-          <button
-            onClick={() => confirmVote(candidate.id, candidate.header)}
-            className="vote-bth"
-            disabled={
-              !this.state.currentVoter.isRegistered ||
-              !this.state.currentVoter.isVerified ||
-              this.state.currentVoter.hasVoted
-            }
-          >
-            Vote
-          </button>
-        </div>
-      </div>
     );
   };
 
   render() {
     if (!this.state.web3) {
       return (
-        <>
-          {this.state.isAdmin ? <NavbarAdmin /> : <Navbar />}
-          <center>Loading Web3, accounts, and contract...</center>
-        </>
+          <>
+            {this.state.isAdmin ? <NavbarAdmin /> : <Navbar />}
+            <center>Loading Web3, accounts, and contract...</center>
+          </>
       );
     }
 
     return (
-      <>
-        {this.state.isAdmin ? <NavbarAdmin /> : <Navbar />}
-        <div>
-          {!this.state.isElStarted && !this.state.isElEnded ? (
-            <NotInit />
-          ) : this.state.isElStarted && !this.state.isElEnded ? (
-            <>
-              {this.state.currentVoter.isRegistered ? (
-                this.state.currentVoter.isHasNFT ? (
-                  this.state.currentVoter.hasVoted ? (
-                    <div className="container-item success">
-                      <div>
-                        <strong>You've casted your vote.</strong>
-                        <p />
-                        <center>
-                          <Link
-                            to="/Results"
-                            style={{
-                              color: "black",
-                              textDecoration: "underline",
-                            }}
-                          >
-                            See Results
-                          </Link>
-                        </center>
-                      </div>
-                    </div>
+        <>
+          {this.state.isAdmin ? <NavbarAdmin /> : <Navbar />}
+          <div>
+            {!this.state.isElStarted && !this.state.isElEnded ? (
+                <NotInit />
+            ) : this.state.isElStarted && !this.state.isElEnded ? (
+                <>
+                  {this.state.currentVoter.isRegistered ? (
+                      this.state.currentVoter.isVerified ? (
+                          this.state.currentVoter.hasVoted ? (
+                              <div className="container-item success">
+                                <div>
+                                  <strong>You've casted your vote.</strong>
+                                  <p />
+                                  <center>
+                                    <Link
+                                        to="/Results"
+                                        style={{
+                                          color: "black",
+                                          textDecoration: "underline",
+                                        }}
+                                    >
+                                      See Results
+                                    </Link>
+                                  </center>
+                                </div>
+                              </div>
+                          ) : (
+                              <div className="container-item info">
+                                <center>Go ahead and cast your vote.</center>
+                              </div>
+                          )
+                      ) : (
+                          <div className="container-item attention">
+                            <center>Please wait for admin to verify.</center>
+                          </div>
+                      )
                   ) : (
-                    <div className="container-item info">
-                      <center>Go ahead and cast your vote.</center>
-                    </div>
-                  )
-                ) : (
-                  <div className="container-item attention">
-                    <center>You do not have NFT. Please check your email.</center>
+                      <>
+                        <div className="container-item attention">
+                          <center>
+                            <p>You're not registered. Please register first.</p>
+                            <br />
+                            <Link
+                                to="/Registration"
+                                style={{ color: "black", textDecoration: "underline" }}
+                            >
+                              Registration Page
+                            </Link>
+                          </center>
+                        </div>
+                      </>
+                  )}
+                  <div className="container-main">
+                    <h2>Candidates</h2>
+                    <small>Total candidates: {this.state.candidates.length}</small>
+                    {this.state.candidates.length < 1 ? (
+                        <div className="container-item attention">
+                          <center>Not one to vote for.</center>
+                        </div>
+                    ) : (
+                        <>
+                          {this.state.candidates.map(this.renderCandidates)}
+                          <div
+                              className="container-item"
+                              style={{ border: "1px solid black" }}
+                          >
+                            <center>That is all.</center>
+                          </div>
+                        </>
+                    )}
                   </div>
-                )
-              ) : (
+                </>
+            ) : !this.state.isElStarted && this.state.isElEnded ? (
                 <>
                   <div className="container-item attention">
                     <center>
-                      <p>You're not registered. Please register first.</p>
+                      <h3>The Election ended.</h3>
                       <br />
                       <Link
-                        to="/Registration"
-                        style={{ color: "black", textDecoration: "underline" }}
+                          to="/Results"
+                          style={{ color: "black", textDecoration: "underline" }}
                       >
-                        Registration Page
+                        See results
                       </Link>
                     </center>
                   </div>
                 </>
-              )}
-              <div className="container-main">
-                <h2>Candidates</h2>
-                <small>Total candidates: {this.state.candidates.length}</small>
-                {this.state.candidates.length < 1 ? (
-                  <div className="container-item attention">
-                    <center>Not one to vote for.</center>
-                  </div>
-                ) : (
-                  <>
-                    {this.state.candidates.map(this.renderCandidates)}
-                    <div
-                      className="container-item"
-                      style={{ border: "1px solid black" }}
-                    >
-                      <center>That is all.</center>
-                    </div>
-                  </>
-                )}
-              </div>
-            </>
-          ) : !this.state.isElStarted && this.state.isElEnded ? (
-            <>
-              <div className="container-item attention">
-                <center>
-                  <h3>The Election ended.</h3>
-                  <br />
-                  <Link
-                    to="/Results"
-                    style={{ color: "black", textDecoration: "underline" }}
-                  >
-                    See results
-                  </Link>
-                </center>
-              </div>
-            </>
-          ) : null}
-        </div>
-      </>
+            ) : null}
+          </div>
+        </>
     );
   }
 }
