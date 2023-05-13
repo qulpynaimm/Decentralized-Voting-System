c9a23aa670ea0b62ec518eed861f4af67e8f5ac3
iamzmeirambek <Iamzmeirambek@gmail.com>
2023-05-14 01:04:54 +0600

    Add candidate and navigation admin

--- client/src/component/Navbar/NavigationAdmin.js
+++ client/src/component/Navbar/NavigationAdmin.js
@@ -2,47 +2,44 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./Navbar.css";
+import Home from "../Home";
+import Logo from "./logo.png";
 
 export default function NavbarAdmin() {
   const [open, setOpen] = useState(false);
   return (
-    <nav>
-      <div className="header">
+      <nav className="header">
         <NavLink to="/">
-          <i className="fab fa-hive" /> Admin
+          <img className="image-icon" onClick={Home} alt="" src={Logo} />
         </NavLink>
-      </div>
-      <ul
-        className="navbar-links"
-        style={{ transform: open ? "translateX(0px)" : "" }}
-      >
-        <li>
-          <NavLink to="/Verification" activeClassName="nav-active">
-            Verification
+        <div className="topmain">
+          <button className="connect-wallet-button">
+            <div className="connect-wallet-button-child" />
+            <div className="connect-wallet">Connect Wallet</div>
+            )}
+          </button>
+          <NavLink to="/about">
+            <div className="about" >
+              About
+            </div>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/AddCandidate" activeClassName="nav-active">
-            Add Candidate
+          <NavLink to="/instructions">
+            <div className="how-to-vote">
+              How to Vote?
+            </div>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Registration" activeClassName="nav-active">
-            <i className="far fa-registered" /> Registration
+          <NavLink to="/voting">
+            <div className="how-to-vote2">
+              Voting
+            </div>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Voting" activeClassName="nav-active">
-            <i className="fas fa-vote-yea" /> Voting
+          <NavLink to="/addCandidate">
+            <div className="add-candidate">
+              Add Candidate
+            </div>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Results" activeClassName="nav-active">
-            <i className="fas fa-poll-h" /> Results
-          </NavLink>
-        </li>
-      </ul>
-      <i onClick={() => setOpen(!open)} className="fas fa-bars burger-menu"></i>
-    </nav>
+          <i onClick={() => setOpen(!open)} className="fas fa-bars burger-menu"></i>
+        </div>
+      </nav>
   );
 }
